a72439aaf6dadffa57206049f5de96894ff50dae
KartikeyMishra <mkartikey670@gmail.com>
2026-03-01 12:52:51 +0530

    Time: 4 ms (60.77%) | Memory: 67.7 MB (20.39%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        vector<int>freq(100001,0);
+        vector<int>freq(nums.size()+1,0);
         for(int i = 0 ; i < nums.size() ; i++){
             freq[nums[i]]++;
             if(freq[nums[i]] > 1) return nums[i];
