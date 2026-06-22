f464a3ce2bd4f2e9855ded2f0d7f6c96a16ba963
Kartikey Mishra <mkartikey670@gmail.com>
2026-06-22 15:46:27 +0530

    Time: 3 ms (61.02%) | Memory: 72.2 MB (74.99%) - LeetSync

--- 1206-corporate-flight-bookings/corporate-flight-bookings.cpp
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
-        vector<int>answer(n,0);
+        vector<int>answer(n+1,0);
         for(int i = 0 ; i < bookings.size() ; i++){
-            int first = bookings[i][0];
-            int last = bookings[i][1];
-            int seats = bookings[i][2];
-            for(int j = first - 1; j < last ; j++){
-                answer[j] += seats;
-            }
+            answer[bookings[i][0]-1] += bookings[i][2];
+            answer[bookings[i][1]] -= bookings[i][2];
         }
+        for(int i = 1 ; i < n ; i++){
+            answer[i] += answer[i-1];
+        }
+        answer.pop_back();
         return answer;
     }
 };
